561819a9e692234b9d5a9ca184bed4d53c1652eb
Marco <patrickstar1281@yahoo.com>
2022-11-12 23:12:17 -0600

    eye test setup

---
+++ script.js
@@ -0,0 +1,7 @@
+const canvas = doctument.getElemtById("canvas1");
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let eyes = [];
+let theta;
+
